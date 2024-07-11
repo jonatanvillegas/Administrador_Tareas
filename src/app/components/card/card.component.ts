5b30d6630e06d519ddd10be4ed67d9baaed9f415
janatanvillegas <villegasjoncena7@gmail.com>
2024-07-10 21:30:06 -0600

    creacion de modelo de tarea y Card

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Tarea } from 'src/app/model/tarea';
 
 @Component({
   selector: 'app-card',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent {
+ Tareas:Tarea[] = [];
+ nuevaTarea:String = "";
 
+  AgregarTare(even:Event){
+    //previniendo el default del submit
+    even.preventDefault();
+    if (this.nuevaTarea.trim() !== "") {
+      const tarea: Tarea = {nombre: this.nuevaTarea, completado: false}
+      this.Tareas.push(tarea)
+      this.nuevaTarea = ""
+    }else{
+      alert("tienes que agregar el nombre a la tarea..")
+    }
+
+  }
 }
+
